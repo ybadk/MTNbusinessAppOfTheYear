8d6a3fc7490320b36aec4535d1243cb82138b7dd
ybadk <kgothatsothooe@gmail.com>
2023-09-11 18:25:20 +0200

    edited scripts

---
+++ mtn-app-academy-Thapelo-Thooe-Module-0/effects.js
@@ -0,0 +1,15 @@
+// Add event listeners to the buttons
+document.getElementById("previous-page").addEventListener("click", function () {
+  // Add your logic to navigate to the previous page
+  // For example, window.location.href = 'previous_page.html';
+});
+
+document.getElementById("homepage").addEventListener("click", function () {
+  // Add your logic to navigate to the homepage
+  // For example, window.location.href = 'index.html';
+});
+
+document.getElementById("next-page").addEventListener("click", function () {
+  // Add your logic to navigate to the next page
+  // For example, window.location.href = 'next_page.html';
+});
